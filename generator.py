ebdb20f5d3bb742210169df4e15b74e0f9142b87
nprodhom <nprodhom@insa-rennes.fr>
2021-09-30 00:01:30 +0200

    début script facture

--- generator.py
+++ generator.py
@@ -1,45 +1,89 @@
 from docx import Document
+from faker import Faker
+import time
+import random
 import os
 
 
 def main():
-    template_file_path = 'employment_agreement_template.docx'
-    output_file_path = 'result.docx'
+    template_file_path = 'templates/test2.docx'
+    output_file_path = 'output/'
 
     variables = {
-        "${EMPLOEE_NAME}": "Example Name",
-        "${EMPLOEE_TITLE}": "Software Engineer",
-        "${EMPLOEE_ID}": "302929393",
-        "${EMPLOEE_ADDRESS}": "דרך השלום מנחם בגין דוגמא",
-        "${EMPLOEE_PHONE}": "+972-5056000000",
-        "${EMPLOEE_EMAIL}": "example@example.com",
-        "${START_DATE}": "03 Jan, 2021",
-        "${SALARY}": "10,000",
-        "${SALARY_30}": "3,000",
-        "${SALARY_70}": "7,000",
+        "${EMPLOEE_NAME}": get_name,
+        "${EMPLOEE_PHONE}": get_phone_number,
+        "${START_DATE}": get_date,
+        "${ADDRESS}": get_address,
+        "${PRIX}": get_prix,
+        "${ITEM}": get_item,
+        "${QUANTITY}": get_quantity,
+        "${COMPANY}": get_company
     }
 
-    template_document = Document(template_file_path)
+    for j in range(0, 20):
+        template_document = Document(template_file_path)
 
-    for variable_key, variable_value in variables.items():
-        for paragraph in template_document.paragraphs:
-            replace_text_in_paragraph(paragraph, variable_key, variable_value)
+        for variable_key, variable_value in variables.items():
+            for paragraph in template_document.paragraphs:
+                replace_text_in_paragraph(paragraph, variable_key, variable_value)
 
-        for table in template_document.tables:
-            for col in table.columns:
-                for cell in col.cells:
-                    for paragraph in cell.paragraphs:
-                        replace_text_in_paragraph(paragraph, variable_key, variable_value)
-
-    template_document.save(output_file_path)
+            for table in template_document.tables:
+                for col in table.columns:
+                    for cell in col.cells:
+                        for paragraph in cell.paragraphs:
+                            replace_text_in_paragraph(paragraph, variable_key, variable_value)
+        template_document.save(output_file_path + template_file_path.split('/')[1].split('.')[0] + str(j + 1) + ".docx")
 
 
 def replace_text_in_paragraph(paragraph, key, value):
     if key in paragraph.text:
         inline = paragraph.runs
+
         for item in inline:
             if key in item.text:
-                item.text = item.text.replace(key, value)
+                item.text = item.text.replace(key, value())
+
+
+def get_phone_number():
+    nb = '+33'
+    for i in range(1, 10):
+        nb = nb + str(random.randint(0, 9))
+    return nb
+
+
+def get_prix():
+    price = random.randrange(10, 1000, 10)
+    return str(price)
+
+
+def get_item():
+    return random.choice(list(open('construction.txt')))
+
+
+def get_quantity():
+    return random.choice(['1', '2', '5', '10', '15', '25', '50'])
+
+
+def get_company():
+    fake = Faker()
+    return fake.company()
+
+
+def get_date():
+    fake = Faker()
+    return fake.date()
+
+
+def get_address():
+    fake = Faker()
+    Faker.seed(random.randint(0, 9999))
+    return fake.address()
+
+
+def get_name():
+    fake = Faker()
+    Faker.seed(random.randint(0, 9999))
+    return fake.name()
 
 
 if __name__ == '__main__':
